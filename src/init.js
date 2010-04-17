cb105b398fc45acd247cf7425ef118c8028b0370
António Afonso <antonio.afonso@gmail.com>
2010-04-17 10:58:22 +0200

    Full working version

---
+++ src/init.js
@@ -0,0 +1,42 @@
+/*
+ * The Original Code is Mozilla Universal charset detector code.
+ *
+ * The Initial Developer of the Original Code is
+ * Netscape Communications Corporation.
+ * Portions created by the Initial Developer are Copyright (C) 2001
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   António Afonso (antonio.afonso gmail.com) - port to JavaScript
+ *   Mark Pilgrim - port to Python
+ *   Shy Shalom - original C code
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ */
+
+var jschardet = {};
+jschardet.VERSION = "0.1";
+jschardet.detect = function(aBuf) {
+    var u = new jschardet.UniversalDetector();
+    u.reset();
+    if( aBuf instanceof Array ) {
+        u.feed(String.fromCharCode.apply(String, aBuf));
+    } else {
+        u.feed(aBuf);
+    }
+    u.close();
+    return u.result;
+}
