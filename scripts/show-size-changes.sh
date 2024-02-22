56e10eefdf378b17c041e61ce7d29a56b863eb07
António Afonso <antonio.afonso@gmail.com>
2024-02-21 18:30:34 -0800

    Cosmetic changes to the way the percentage is shown in show-size-changes.sh

--- scripts/show-size-changes.sh
+++ scripts/show-size-changes.sh
@@ -32,7 +32,7 @@ eval "git diff-index HEAD $@" | {
     # Calculate the percentage up to 2 decimal places and leading 0 when the
     # percentage only has the decimal part. Only print percentages >= 0.01%.
     bc_expr="scale=2; v=$size_diff/$sizeB"
-    bc_expr=$bc_expr'; if (v >= 0.01) { if (v < 0) { print "-" } else { print "+" }; if (abs(v) < 1) print "0"; print abs(v) }; print ""'
+    bc_expr=$bc_expr'; if (v >= 0.01) { if (v < 0) { print "-" } else { print "+" }; if (abs(v) < 1) print "0"; print abs(v); print "% " }; print ""'
     perc=$(bc -e "$bc_expr")
     echo -e "$path $size_diff_color$size_diff_signal$size_diff $perc$TC($sizeB -> $sizeA)"
   done
