25f107a331f27c51b3436e1132e444c89ecf596f
António Afonso <antonio.afonso@gmail.com>
2024-03-12 19:38:19 -0700

    Updated github workflow files

--- scripts/show-size-changes.sh
+++ scripts/show-size-changes.sh
@@ -5,6 +5,28 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 TC='\033[0m' # Terminal color
 
+function calc_perc {
+  # Calculate the percentage up to 2 decimal places and leading 0 when the
+  # percentage only has the decimal part. Only print percentages >= 0.01%.
+  bc <<EOF
+    scale=2;
+    define abs2(x) { if (x < 0) return -x; return x }
+    ratio=$1/$2;
+    if (ratio >= 0.01) {
+      if (ratio < 0) {
+        print "-"
+      } else {
+        print "+"
+      }
+      if (abs2(ratio) < 1) {
+        print "0"
+      }
+      print abs2(ratio)
+      print "% "
+    }
+EOF
+}
+
 eval "git diff-index HEAD $@" | {
   # vars: B=before / A=after
   # mode: A=added / D=deleted
@@ -31,9 +53,10 @@ eval "git diff-index HEAD $@" | {
 
     # Calculate the percentage up to 2 decimal places and leading 0 when the
     # percentage only has the decimal part. Only print percentages >= 0.01%.
-    bc_expr="scale=2; v=$size_diff/$sizeB"
-    bc_expr=$bc_expr'; if (v >= 0.01) { if (v < 0) { print "-" } else { print "+" }; if (abs(v) < 1) print "0"; print abs(v); print "% " }; print ""'
-    perc=$(bc -e "$bc_expr")
+    # bc_expr="scale=2; v=$size_diff/$sizeB"
+    # bc_expr=$bc_expr'; if (v >= 0.01) { if (v < 0) { print "-" } else { print "+" }; if (abs(v) < 1) print "0"; print abs(v); print "% " }; print ""'
+    # perc=$(echo "$bc_expr" | bc)
+    perc=$(calc_perc $size_diff $sizeB)
     echo -e "$path $size_diff_color$size_diff_signal$size_diff $perc$TC($sizeB -> $sizeA)"
   done
 }
